468a422f43aaf63e7819110f5d4668326232bf45
Bausson Etienne <bausson.etienne@gmail.com>
2012-04-21 14:01:52 +0200

    Now serving files from static directory.

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 var util = require("util");
 var serialport = require("serialport");
+var http = require('http');
+var static = require('node-static');
 
-console.log(serialport);
+
+// getting data from serial
 
 var serial = new serialport.SerialPort("/dev/ttyUSB0", { 
   parser: serialport.parsers.readline("\n")
@@ -15,3 +18,11 @@ serial.on("data", function (data) {
     lastData = data;
   }
 });
+
+// static file server
+var file = new(static.Server)('./static');
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  });
+}).listen(8080);
